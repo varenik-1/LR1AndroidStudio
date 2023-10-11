1346c2b3e5d2aa4ebfc0ed6f4f64acff19110a02
varenik-1 <vs90288vs@gmail.com>
2023-10-11 17:52:46 +0300

    add project hz kak zit' uze

---
+++ lab1/app/src/main/java/mai/team4/cowsssssss/MainActivity.java
@@ -0,0 +1,14 @@
+package mai.team4.cowsssssss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
